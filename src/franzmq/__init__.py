f6884877c582f42fefb50eeb917ecc1f8ceb0852
Till Schöpe <schoepe@alpamayo.ch>
2025-10-08 11:05:13 +0200

    remove utils

--- src/franzmq/__init__.py
+++ src/franzmq/__init__.py
@@ -2,4 +2,7 @@ from .client import Client
 from .topic import Topic
 from .message import Message
 from .data_contracts.base import Payload
+import json
+import enum
+
 __all__ = ["Payload", "Message", "Topic", "Client"]
