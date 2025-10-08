4834f1b47457c5824ebf57623545b6a2175e9328
Walther <walther.trgovac@outlook.com>
2025-10-08 11:22:18 +0200

    add all classes and functions for importing

--- src/franzmq/__init__.py
+++ src/franzmq/__init__.py
@@ -1,8 +1,7 @@
 from .client import Client
-from .topic import Topic
+from .topic import Topic, Isa95Fields, Isa95Topic
 from .message import Message
-from .data_contracts.base import Payload
-import json
-import enum
+from .data_contracts.base import Payload, Metric, Log
+from .log_handlers import configure_logging
 
-__all__ = ["Payload", "Message", "Topic", "Client"]
+__all__ = ["Payload", "Message", "Topic", "Client", "Isa95Fields", "Isa95Topic", "Metric", "Log", "configure_logging"]
