274dcec68bd03fc77b4eddd80f021f4c80abf176
Christoph Netsch <netsch@alpamayo.ch>
2026-02-22 17:03:42 +0100

    ```

--- src/franzmq/__init__.py
+++ src/franzmq/__init__.py
@@ -1,7 +1,14 @@
 from .client import Client
 from .topic import Topic, Isa95Fields, Isa95Topic
+from .topic_base import TopicBase, classproperty
 from .message import Message
-from .data_contracts.base import Payload, Metric, Log
+from .data_contracts.base import Payload, Metric, Log, Cmd, Ack
 from .log_handlers import configure_logging
 
-__all__ = ["Payload", "Message", "Topic", "Client", "Isa95Fields", "Isa95Topic", "Metric", "Log", "configure_logging"]
+__all__ = [
+    "Payload", "Message", "Topic", "Client",
+    "Isa95Fields", "Isa95Topic",
+    "Metric", "Log", "Cmd", "Ack",
+    "TopicBase", "classproperty",
+    "configure_logging",
+]
